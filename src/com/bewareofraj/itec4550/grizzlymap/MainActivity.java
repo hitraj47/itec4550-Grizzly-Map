01ea9be4410f42cfe2b7b08df98b39c87bc565a1
Raj Ramsaroop <raj@bewareofraj.com>
2014-09-02 01:42:25 -0400

    add onclicklistener to area map button

--- src/com/bewareofraj/itec4550/grizzlymap/MainActivity.java
+++ src/com/bewareofraj/itec4550/grizzlymap/MainActivity.java
@@ -1,13 +1,33 @@
 package com.bewareofraj.itec4550.grizzlymap;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
+	
+	private ImageButton btnAreaMap;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		initializeButtons();
+	}
+
+	private void initializeButtons() {
+		btnAreaMap = (ImageButton) findViewById(R.id.btnAreaMap);
+		btnAreaMap.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent areaMapIntent = new Intent(MainActivity.this, AreaMapActivity.class);
+				startActivity(areaMapIntent);
+			}
+		});
 	}
 }
